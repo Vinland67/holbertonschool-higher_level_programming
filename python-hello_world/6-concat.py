85f626045e5195354b485a2339d564ada19cc075
Vinland67 <s13626@holbertonstudents.com>
2026-03-21 10:52:37 +0000

    Standard variable-based concatenation

--- python-hello_world/6-concat.py
+++ python-hello_world/6-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str1 = "Welcome to"
-str2 = "Holberton School"
-str1 += " " + str2
-print(f"Welcome to {str2}!")
+str2 = "Battery Street"
+str1 = str1 + " " + str2
+print(f"{str1}!")
