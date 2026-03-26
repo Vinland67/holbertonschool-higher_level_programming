cb9066a29bcdb28d4a0e818df78cf689419035e6
Vinland67 <13626@holbertonstudents.com>
2026-03-26 20:59:31 +0000

    Find the biggest integer of a list

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if not my_list:
+        return None
+    
+    big_num = my_list[0]
+    for i in my_list:
+        if i > big_num:
+            big_num = i
+    return big_num
