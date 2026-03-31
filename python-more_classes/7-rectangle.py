22464946c9f20bad1c6c2393948ec077e28dc3c7
Vinland67 <13626@holbertonstudents.com>
2026-03-31 22:02:42 +0000

    Change representation - Added print_symbol

---
+++ python-more_classes/7-rectangle.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python3
+"""Çap simvolu dəyişdirilə bilən Rectangle klassı."""
+
+
+class Rectangle:
+    """Düzbucaqlını təmsil edən klass.
+
+    Attributes:
+        number_of_instances (int): Yaradılmış obyektlərin sayı.
+        print_symbol (any): Çap üçün istifadə olunan simvol.
+    """
+
+    number_of_instances = 0
+    print_symbol = "#"
+
+    def __init__(self, width=0, height=0):
+        """Düzbucaqlını yaradır."""
+        self.width = width
+        self.height = height
+        Rectangle.number_of_instances += 1
+
+    @property
+    def width(self):
+        """Width getter."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Width setter."""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Height getter."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Height setter."""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Sahəni hesablayır."""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """Perimetri hesablayır."""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
+
+    def __str__(self):
+        """Düzbucaqlını print_symbol ilə vizual təsvir edir."""
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        
+        # Simvolu stringə çevirib sətirləri qururuq
+        row = str(self.print_symbol) * self.__width
+        rect_str = (row + "\n") * self.__height
+        return rect_str[:-1]  # Sonuncu artıq yeni sətiri (\n) silirik
+
+    def __repr__(self):
+        """Obyektin kod təsvirini qaytarır."""
+        return "Rectangle({}, {})".format(self.__width, self.__height)
+
+    def __del__(self):
+        """Obyekt silinərkən mesaj çap edir və sayğacı azaldır."""
+        Rectangle.number_of_instances -= 1
+        print("Bye rectangle...")
