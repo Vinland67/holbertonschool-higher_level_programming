b55ac62adad500bf53861502997e01951832f6eb
Vinland67 <13626@holbertonstudents.com>
2026-04-06 20:34:05 +0000

    Task 9: Full rectangle with area and __str__ methods

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Module for Rectangle class that inherits from BaseGeometry.
+"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """A class representing a rectangle using BaseGeometry."""
+
+    def __init__(self, width, height):
+        """
+        Initialize the rectangle with width and height.
+
+        Args:
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+        """
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        """Returns the area of the rectangle."""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """Returns a string representation of the rectangle."""
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
