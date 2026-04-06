ad5651f7eee25742a1334291e017e5dff2efff47
Vinland67 <13626@holbertonstudents.com>
2026-04-06 20:35:10 +0000

    Task 11: Square class with its own __str__ method

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Module for Square class that inherits from Rectangle.
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """A class representing a square using Rectangle."""
+
+    def __init__(self, size):
+        """
+        Initialize the square with size.
+
+        Args:
+            size (int): The size of the square's side.
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def __str__(self):
+        """Returns a string representation of the square."""
+        return "[Square] {}/{}".format(self.__size, self.__size)
