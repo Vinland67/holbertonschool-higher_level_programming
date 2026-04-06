9510307bb7ae396c48413b10f472f0382cf10e8e
Vinland67 <13626@holbertonstudents.com>
2026-04-06 20:26:16 +0000

    Task 6: Added area method that raises Exception

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+BaseGeometry modulunu təkmilləşdiririk.
+"""
+
+
+class BaseGeometry:
+    """BaseGeometry klası."""
+
+    def area(self):
+        """
+        Sahəni hesablamalıdır, lakin hələ tətbiq edilməyib.
+        
+        Raises:
+            Exception: area() is not implemented mesajı ilə.
+        """
+        raise Exception("area() is not implemented")
