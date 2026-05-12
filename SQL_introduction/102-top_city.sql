d540dcc410f0efeb07a4c634cfabbd5bf307523b
Vinland67 <13626@holbertonstudents.com>
2026-05-12 21:33:59 +0000

    Task 19: Top 3 cities for July and August

--- SQL_introduction/102-top_city.sql
+++ SQL_introduction/102-top_city.sql
@@ -1,4 +1,4 @@
--- Displays the top 3 of cities temperature during July and August 
+-- Displays the top 3 of cities temperature during July and August
 -- ordered by temperature (descending)
 SELECT city, AVG(value) AS avg_temp
 FROM temperatures
